07629e7d44d7833efd07392d30b4e34597df0760
shubhin151 <60954538+shubh-151@users.noreply.github.com>
2023-10-31 21:05:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import './App.css'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>Learn about redux tollkit </h1>
+     <AddTodo/>
+    <Todos/>
+   
+    </>
+  )
+}
+
+export default App
